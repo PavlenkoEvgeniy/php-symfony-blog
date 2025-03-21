6e0fa80e56a9d77524faa0b4094e2c8254c4217a
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-03-21 15:07:51 -0400

    add trash, restore and truncate for posts

--- src/Controller/Admin/PostController.php
+++ src/Controller/Admin/PostController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/post')]
+
 final class PostController extends AbstractController
 {
-    #[Route(name: 'app_admin_post_index', methods: ['GET'])]
+    #[Route('/admin/post', name: 'app_admin_post_index', methods: [Request::METHOD_GET])]
     public function index(PostRepository $postRepository): Response
     {
         return $this->render('admin/post/index.html.twig', [
@@ -22,7 +22,7 @@ final class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_post_new', methods: ['GET', 'POST'])]
+    #[Route('/admin/post/new', name: 'app_admin_post_new', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $post = new Post();
@@ -42,7 +42,7 @@ final class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_post_show', methods: ['GET'])]
+    #[Route('/admin/post/show/{id}', name: 'app_admin_post_show', methods: [Request::METHOD_GET])]
     public function show(Post $post): Response
     {
         return $this->render('admin/post/show.html.twig', [
@@ -50,7 +50,7 @@ final class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_post_edit', methods: ['GET', 'POST'])]
+    #[Route('/admin/post/edit/{id}', name: 'app_admin_post_edit', methods: [Request::METHOD_GET, Request::METHOD_POST])]
     public function edit(Request $request, Post $post, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostType::class, $post);
@@ -68,7 +68,7 @@ final class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_post_delete', methods: ['POST'])]
+    #[Route('/admin/post/delete/{id}', name: 'app_admin_post_delete', methods: [Request::METHOD_DELETE])]
     public function delete(Request $request, Post $post, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->getPayload()->getString('_token'))) {
@@ -77,5 +77,48 @@ final class PostController extends AbstractController
         }
 
         return $this->redirectToRoute('app_admin_post_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/admin/post/trash', name: 'app_admin_post_trash', methods: [Request::METHOD_GET])]
+    public function trash(PostRepository $postRepository, EntityManagerInterface $entityManager): Response
+    {
+        $entityManager->getFilters()->disable('softDeleteable');
+
+        return $this->render('admin/post/trash.html.twig', [
+            'posts' => $postRepository->findDeletedPosts(),
+        ]);
+    }
+
+    #[Route('/admin/post/restore/{id}', name: 'app_admin_post_restore', methods: [Request::METHOD_GET])]
+    public function restore(PostRepository $postRepository, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $entityManager->getFilters()->disable('softDeleteable');
+
+        $post = $postRepository->findOneById($id);
+
+        if (!$post) {
+            throw $this->createNotFoundException('Пост не найден.');
+        }
+
+        $post->setDeletedAt(null);
+    
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_admin_post_trash', [], Response::HTTP_SEE_OTHER);
+    }
+
+    #[Route('/admin/post/truncate/{id}', name: 'app_admin_post_truncate', methods: [Request::METHOD_DELETE])]
+    public function truncate(Request $request, PostRepository $postRepository, EntityManagerInterface $entityManager, int $id): Response
+    {
+        if ($this->isCsrfTokenValid('truncate'.$id, $request->getPayload()->getString('_token'))) {
+            $entityManager->getFilters()->disable('softDeleteable');
+
+            $post = $postRepository->findOneById($id);
+
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_admin_post_trash', [], Response::HTTP_SEE_OTHER);
     }
 }
