3fd6a6ff788726901754249902ec4be45cb1284b
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-03-21 15:08:24 -0400

    add post form attributes

--- src/Form/PostType.php
+++ src/Form/PostType.php
@@ -6,22 +6,36 @@ use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class PostType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('content')
-            ->add('isPublished')
+            ->add('title', TextType::class, [
+                'label' => 'Название',
+                'required' => true,
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Содержание',
+                'required' => true,
+                'attr' => [
+                    'rows' => 10,
+                ]
+            ])
+            ->add('isPublished', CheckboxType::class, [
+                'label' => 'Опубликован',
+                'required' => false,
+            ])
             ->add('createdAt', null, [
+                'label' => 'Дата создания',
                 'widget' => 'single_text',
             ])
             ->add('updatedAt', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('deletedAt', null, [
+                'label' => 'Дата обновления',
                 'widget' => 'single_text',
             ])
         ;
